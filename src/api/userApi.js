b27c19ed70146a6f13217874a77e6e75d0257f7e
Benjamin Holst <bepahol2@gmail.com>
2020-03-11 23:23:29 -0600

    module 11 - HTTP calls

---
+++ src/api/userApi.js
@@ -0,0 +1,38 @@
+import 'whatwg-fetch';
+import getBaseUrl from './baseUrl';
+
+const baseUrl = getBaseUrl();
+
+export function getUsers() {
+    return get('users');
+}
+
+export function deleteUser() {
+    return del(`users/${id}`);
+}
+
+function get(url) {
+    return fetch(baseUrl + url).then(onSuccess, onError); // this isn't working...
+    /* console.log( "get: " + fetch(url).then(onSuccess, onError) );
+    return [
+        {"id": 1, "firstName":"Bob",   "lastName":"Smith",  "email":"bob@gmail.com"},
+        {"id": 2, "firstName":"Tammy", "lastName":"Norton", "email":"tnorton@yahoo.com"}
+    ];*/
+}
+
+// Can't name the function "delete" since it's a reserved word in js
+function del(url) {
+    const request = new Request(baseUrl + url, {
+        method: 'DELETE'
+    });
+
+    return fetch(request).then(onSuccess, onError);
+}
+
+function onSuccess(response) {
+    response.json();
+}
+
+function onError(error) {
+    console.log(error); // eslint-disable-line no-console
+}
